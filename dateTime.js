7501af655604728f99532ede00b8d16b3e5f9934
Yoanm <yoanm@users.noreply.github.com>
2016-11-12 04:08:47 +0100

    Use npm + add test + add scrutinizer + add eslint

--- dateTime.js
+++ dateTime.js
@@ -1,60 +1,62 @@
+/* global Config */
 /**
  * @summary Sarah endpoint for dateTime plugin
  *
- * @requires dateTimeModule
- * @requires sarahLogger
- * @requires sarahActionContext
- * @requires sarahActionHelper
- * @requires sarahVersion
+ * @requires dateTimePlugin
+ * @requires sarah-lib-utils/logger
+ * @requires sarah-lib-utils/version
+ * @requires sarah-lib-utils/actionContext
+ * @requires sarah-lib-utils/actionHelper
  */
-
-const SarahLogger = require('sarahLogger');
-const SarahActionContext = require('sarahActionContext');
-const SarahActionHelper = require('sarahActionHelper');
-const DateTimeModule = require('dateTimeModule');
-const version = require('sarahVersion');
+const SarahLogger = require('sarah-lib-utils/logger');
+const version = require('sarah-lib-utils/version');
+const SarahActionContext = require('sarah-lib-utils/actionContext');
+const SarahActionHelper = require('sarah-lib-utils/actionHelper');
+const DateTimePlugin = require('./lib/dateTimePlugin');
 
 /**
  * SARAH server plugin init function. Could be called at server startup or if plugin is reloaded
  *
  * @param {SARAH} SARAH
  */
-exports.init = function(SARAH)
-{
+exports.init = function(SARAH) {// eslint-disable-line no-unused-vars
     this.logger = new SarahLogger('DateTime');
     this.logger.info('initialization ...');
 
-    this.module = new DateTimeModule(
-        version.isV4()
-            ? getYearOnDateConfig(Config)
-            : true
+    this.plugin = new DateTimePlugin(
+        version.isV3()
+            ? true
+            : getYearOnDateConfig(Config)
     );
-    this.logger.info(this.module.getDateTimeMessage());
+    this.logger.info(this.plugin.getDateTimeMessage());
 
     this.logger.info('initialized !');
 };
 /**
  * SARAH server plugin dispose function. Called before plugin will be reloaded
  */
-exports.dispose = function(){
-    this.module = null;
+exports.dispose = function() {
+    this.plugin = null;
     this.logger.info('uninitialized !');
     this.logger = null;
 };
 /**
  * SARAH server plugin action function. Called when SARAH client reach the server at plugin endpoint
- * {@inheretedDoc}
+ * @param {object} data
+ * @param {callable} callback
+ * @param {Config} config
+ * @param {SARAH} SARAH
  */
-exports.action = function (data, callback, config, SARAH) {
+exports.action = function(data, callback, config, SARAH) {
     var context = new SarahActionContext(data, callback);
     var helper = new SarahActionHelper(context);
     if (version.isV3()) {
         context.setSARAH(SARAH);
-        this.module.setYearOnDate(
+        this.plugin.setYearOnDate(
             getYearOnDateConfig(config)
         );
     }
-    this.module.fromAction(data.action, helper);
+    this.plugin.fromAction(data.action, helper);
 };
 
 /**
@@ -63,5 +65,5 @@ exports.action = function (data, callback, config, SARAH) {
  * @returns {boolean}
  */
 function getYearOnDateConfig(config) {
-    return true == config.modules.dateTime.yearOnDate;
+    return config.modules.dateTime.yearOnDate === true;
 }
